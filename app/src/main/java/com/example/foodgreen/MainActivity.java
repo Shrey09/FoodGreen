191c1f40f602f67555fd4d7f9ea16197ace216a2
Shrey Amin <shreyberba09@gmail.com>
2019-02-28 17:32:15 -0400

    Initial home UI design

--- app/src/main/java/com/example/foodgreen/MainActivity.java
+++ app/src/main/java/com/example/foodgreen/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.foodgreen;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    android.support.v7.widget.Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=(android.support.v7.widget.Toolbar) findViewById(R.id.new_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Food Green");
+        toolbar.setTitleTextColor(Color.BLACK);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
     }
 }
