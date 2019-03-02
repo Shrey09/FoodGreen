47d8b2bf34e8f6e0cd9b5d7a61bcabbcdbdf3eb0
Chintan Patel <chintan97patel@gmail.com>
2019-03-02 05:48:42 -0400

    Initial UI of buy and sell pages

---
+++ app/src/main/java/com/example/foodgreen/BuyView.java
@@ -0,0 +1,88 @@
+package com.example.foodgreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class BuyView extends AppCompatActivity {
+
+    ListView food;
+    String[] dish={"first","Second","Third","Fourth","first","Second","Third","Fourth"};
+    String [] price={"22","23","24","25","22","23","24","25"};
+    Integer [] quantity={2,3,4,5,2,3,4,5};
+    String [] location={"qunipool","spring","sexton","spring","qunipool","spring","sexton","spring"};
+    Integer[] images={R.drawable.food,R.drawable.food2,R.drawable.food3,R.drawable.food,R.drawable.food2,R.drawable.food3,R.drawable.food,R.drawable.food2};
+    android.support.v7.widget.Toolbar toolbar;
+    ImageView homeButton, sellButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buyview);
+        food=(ListView) findViewById(R.id.foodlist);
+        toolbar=(android.support.v7.widget.Toolbar) findViewById(R.id.new_bar);
+        setSupportActionBar(toolbar);
+        CustomListView customListView=new CustomListView();
+        food.setAdapter(customListView);
+
+        homeButton = findViewById(R.id.homebtn);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent home_intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(home_intent);
+            }
+        });
+
+        sellButton = findViewById(R.id.sellbtn);
+        sellButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sell_intent = new Intent(v.getContext(), SellView.class);
+                startActivity(sell_intent);
+            }
+        });
+    }
+    class CustomListView extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View view=getLayoutInflater().inflate(R.layout.buy_list_layout,null);
+
+            ImageView mImageView=(ImageView)view.findViewById(R.id.imageview);
+            TextView  dishname=(TextView) view.findViewById(R.id.dishname);
+            TextView  priceval=(TextView) view.findViewById(R.id.valprice);
+            TextView  quantityval=(TextView) view.findViewById(R.id.valquantity);
+            TextView  locationval=(TextView) view.findViewById(R.id.vallocation);
+
+            mImageView.setImageResource(images[position]);
+            dishname.setText(dish[position]);
+            priceval.setText(price[position]);
+            quantityval.setText(String.valueOf(quantity[position]));
+            locationval.setText(location[position]);
+            return view;
+        }
+    }
+}
